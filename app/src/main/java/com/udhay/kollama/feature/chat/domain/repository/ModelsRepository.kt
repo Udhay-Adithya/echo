68fae500b333cc96e757299aa213ec92f1d1d493
Udhay-Adithya <udhayxd@gmail.com>
2026-05-10 23:56:08 +0530

    feat: add chat and chat stream functions

--- app/src/main/java/com/udhay/kollama/feature/chat/domain/repository/ModelsRepository.kt
+++ app/src/main/java/com/udhay/kollama/feature/chat/domain/repository/ModelsRepository.kt
@@ -1,10 +1,17 @@
 package com.udhay.kollama.feature.chat.domain.repository
 
 import com.udhay.kollama.feature.chat.domain.model.OllamaModel
+import kotlinx.coroutines.flow.Flow
+import org.udhay.ollama.api.ChatRequest
+import org.udhay.ollama.api.ChatResponse
 import org.udhay.ollama.api.ListResponse
 
 interface ModelsRepository {
     suspend fun getModels() : List<OllamaModel>
 
     suspend fun getStatus() : Boolean
+
+    suspend fun chat(chatRequest: ChatRequest): ChatResponse
+
+    suspend fun chatStream(chatRequest: ChatRequest): Flow<ChatResponse>
 }
